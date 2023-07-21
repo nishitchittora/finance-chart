e68c73b99c2d877ea5a348b6f2fdfb0b6884ac5e
Nishit Chittora <nishitchittora@Nishits-MacBook-Pro-2.local>
2023-07-21 12:14:31 +0530

    backed added

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3002;
+const router = require("./routes/routes");
+
+app.use(router);
+
+app.listen(PORT, function (err) {
+	if (err) console.log(err);
+	console.log("Server listening on PORT", PORT);
+});
---
+++ backend/routes/routes.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+var fs = require("fs");
+
+var yahoo = require("../service/stockprice.js");
+
+router.post("/get", function (req, res, next) {
+	const symbol = req.body.symbol;
+	const range = req.body.range;
+
+	yahoo.getOneStockBySymbol(symbol, range).then((data) => {
+		if (data) {
+			res.json(data);
+		} else {
+			res.status(404).send();
+		}
+	});
+});
+
+module.exports = router;
